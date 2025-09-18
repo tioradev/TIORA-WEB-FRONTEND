a2b1447ef36485bafb821f4aff52abc9774435f0
Ammar Ahamed <ammar_a@epic.lk>
2025-09-18 13:29:37 +0530

    test ipg

--- src/components/owner/PaymentBilling.tsx
+++ src/components/owner/PaymentBilling.tsx
@@ -260,7 +260,7 @@ const PaymentBilling: React.FC = () => {
         billingAddressStreet: 'N/A',
         billingAddressCity: salon?.district || 'Colombo',
         billingAddressCountry: 'LKA',
-        billingAddressStateProvince: 'N/A',
+        billingAddressStateProvince: 'Western',
         billingAddressPostcodeZip: salon?.postalCode || '00100'
       };
 
